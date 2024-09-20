0d1b52cb6c0ee857c3d93e8ebeaf293119e96e29
Nicolas Martinez-Wise <s2404148@ed.ac.uk>
2024-09-20 16:08:18 +0100

    Initial commit

---
+++ scurvy_analysis.R
@@ -0,0 +1,16 @@
+library(readr)
+scurvy <- read_csv("scurvy.csv")
+
+summary(scurvy)
+
+scurvy$gum_rot_d6 <- gsub("_.*", "", scurvy$gum_rot_d6)
+scurvy$weakness_of_the_knees_d6 <- gsub("_.*", "", scurvy$weakness_of_the_knees_d6)
+scurvy$skin_sores_d6 <- gsub("_.*", "", scurvy$skin_sores_d6)
+
+columns_to_change <- c("lassitude_d6", "fit_for_duty_d6")
+for (i in columns_to_change) { scurvy[[i]] <- gsub("_.*", "", scurvy[[i]])
+}
+
+#the below is wrong
+count <- table(scurvy$gum_rot_d6, scurvy$treatment)
+barplot(count)
